bb1e120723bd320c5f4df41fb4ead2df1a687c72
Danny Graham <good.midget@gmail.com>
2015-01-12 10:56:36 -0800

    brought logging into our ftp helper; also cleaned up the ftp helper to bring it in line with our little code standard we have going on here

--- release/lib/helpers/transports/ftp.js
+++ release/lib/helpers/transports/ftp.js
@@ -1,141 +1,162 @@
+// Copyright Impact Marketing Specialists, Inc. and other contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var FTP = exports;
+
+/** log facility */
+var log = require('debug')('HoneyBadger:Helper:FTP');
+
+/** core deps */
+var util = require('util');
+var url = require('url');
+var events = require('events');
 var libftp = require('ftp');
+var stream = require('stream');
 
-Object.defineProperties(module.exports,{
-	validate: {
-		value: function(source, callback){
-	        var c = new libftp();
+FTP.validate = function(source, callback){
+	var c = new libftp();
 
-	        c.on('ready', function() {
-	            c.list(function(err, list) {
-	              if (err) {
-	                process.nextTick(function(){
-	                    callback(err,null);
-	                });
-	                return;
-	              }
-	              c.end();
-	              process.nextTick(function(){
-	                callback(null,{success:true})
-	              })
-	            });
-	        });
+	log('Validating connection to source');
+	c.on('ready', function() {
+		log('Connection success');
+		c.end();
+		process.nextTick(function(){
+			callback(null,{success:true})
+		})
+	});
 
-	        c.on('error', function(err) {
-	            console.trace(err);
-	            process.nextTick(function(){
-	                callback(err, null);
-	            });
-	        });
+	c.on('error', function(err) {
+		log('Connection error');
+		console.trace(err);
+		process.nextTick(function(){
+			callback(err, null);
+		});
+	});
 
-	        c.connect({
-	            host: source.uri,
-	            port: source.port,
-	            user: source.auth.username,
-	            password: source.auth.password
-	        });
+	c.connect({
+		host: source.uri,
+		port: source.port,
+		user: source.auth.username,
+		password: source.auth.password
+	});
+};
 
-		},
-		enumerable: true
-	},
-	browse: {
-		value: function(source, callback){
-			var c = new libftp();
+FTP.browse = function(source, callback){
+	var c = new libftp();
 
-			var basepath = source.basepath || '/';
+	var basepath = source.basepath || '/';
 
-			c.on('ready', function() {
-				c.list(basepath, function(err, list) {
-					c.end();
-					process.nextTick(function(){
-						callback(err, list);
-					});
-				});
+	log('Browsing source');
+
+	c.on('ready', function() {
+		c.list(basepath, function(err, list) {
+			log('Returning file list');
+			c.end();
+			process.nextTick(function(){
+				callback(err, list);
 			});
+		});
+	});
 
-			c.on('error', function(err) {
-				console.trace(err);
-				process.nextTick(function(){
-					callback(err, null);
-				});
-			});
+	c.on('error', function(err) {
+		log('Error browsing source');
+		console.trace(err);
+		process.nextTick(function(){
+			callback(err, null);
+		});
+	});
 
-			c.connect({
-				host: source.uri,
-				port: source.port,
-				user: source.auth.username,
-				password: source.auth.password
-			});
-		},
-		enumerable: true
-	},
-	get: {
-		value: function(source, target, callback){
-			var c = new libftp();
+	c.connect({
+		host: source.uri,
+		port: source.port,
+		user: source.auth.username,
+		password: source.auth.password
+	});
+};
 
-			c.on('ready', function() {
-				c.get(target, function(err, stream){
-					if (!err) stream.once('close', function(){
-						console.log('closing ftp data stream');
-						c.end(); 
-					});
-					// process.nextTick(function(){
-					if (callback) callback(err, stream);
-					// });
-				});
-			});
+FTP.get = function(source, target, callback){
+	var c = new libftp();
 
-			c.on('error', function(err) {
-				console.trace(err);
-				process.nextTick(function(){
-					callback(err, null);
-				});
+	log('Fetching source target');
+	c.on('ready', function() {
+		c.get(target, function(err, stream){
+			if (!err) stream.once('close', function(){
+				log('Data stream closed');
+				c.end(); 
 			});
+			// process.nextTick(function(){
+			if (callback) callback(err, stream);
+			// });
+		});
+	});
 
-			c.connect({
-				host: source.uri,
-				port: source.port,
-				user: source.auth.username,
-				password: source.auth.password
-			});
-		},
-		enumerable: true
-	},
-	put: {
-		value: function(stream, source, target, callback){
-			var Client = require('ftp');
-			var c = new Client();
+	c.on('error', function(err) {
+		log('Error fetching source target');
+		console.trace(err);
+		process.nextTick(function(){
+			callback(err, null);
+		});
+	});
 
-			stream.on('finish', function(){
-				// console.log('outstream_finished')
-				// c.end();
-			});
+	c.connect({
+		host: source.uri,
+		port: source.port,
+		user: source.auth.username,
+		password: source.auth.password
+	});
+};
 
-			c.on('ready', function() {
-				c.put(stream, target, function(err){
-					c.end();
-				});
-			});
+FTP.put = function(stream, source, target, callback){
+	var c = new libftp();
 
-			c.on('close', function(){
-				console.log('complete');
-				c.destroy();
-				callback(null, true);
-			});
+	log('Putting file to target');
+	stream.on('finish', function(){
+		log('Incoming data stream finished');
+	});
 
-			c.on('error', function(err) {
-				console.trace(err);
-				process.nextTick(function(){
-					callback(err, null);
-				});
-			});
+	c.on('ready', function() {
+		log('Streaming data to output target');
+		c.put(stream, target, function(err){
+			c.end();
+		});
+	});
 
-			c.connect({
-				host: source.uri,
-				port: source.port,
-				user: source.auth.username,
-				password: source.auth.password
-			});
-		},
-		enumerable: true
-	}
-});
+	c.on('close', function(){
+		log('Completed writing to remote target; connection closed');
+		c.destroy();
+		callback(null, true);
+	});
+
+	c.on('error', function(err) {
+		log('Error writing to remote target')
+		console.trace(err);
+		process.nextTick(function(){
+			callback(err, null);
+		});
+	});
+
+	c.connect({
+		host: source.uri,
+		port: source.port,
+		user: source.auth.username,
+		password: source.auth.password
+	});
+};
