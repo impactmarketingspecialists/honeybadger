e8f6e40fcd1729d6e66608cbb226290221c148a2
Danny Graham <good.midget@gmail.com>
2015-01-20 09:39:34 -0800

    turn back on data select

--- release/index.js
+++ release/index.js
@@ -1,5 +1,5 @@
 var HoneyBadger = require('./honeybadger').Service;
-// var select = require('./data-select'); // This will soon follow the model above
+var select = require('./data-select'); // This will soon follow the model above
 var admin = require('./admin/admin'); // This will stay the same; no intent for folks to include admin in their sources
 
 HoneyBadger.main();
